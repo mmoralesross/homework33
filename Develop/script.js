abc0d2d929730bfe7f2fb9bf90028dff57b4de31
“mmoralesross” <mmoralesross@gmail.com>
2020-10-17 19:12:54 -0400

    testing generate button

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,29 @@
 // Assignment Code
+var enter;
+var numbers;
+var characters;
+var uppercases;
+var lowercases
+
+characters = ["!", "@", "$", "%", "^", "&", "*", "(", ")", "-", "+"];
+
+number = [1,2, 3, 4, 5, 6, 7, 8, 9,10];
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+space = [];
+
+var choices; 
+
+var toUpper = function (x) {
+  return x.toUpperCase();
+
+};
+
+letters2 = letters.map(toUpper);
+
+var generateBtn = document.querySelector("#generate");
+
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var dogName = prompt ("What is your dogs name?") 
